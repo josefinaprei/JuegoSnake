3489cde5272b70c79781f171ba55da1f9b8671cf
Josefina Preiser <134456477+josefinaprei@users.noreply.github.com>
2025-08-13 12:48:12 -0300

    Se crearon llas funciones drawSanke() y updateScore() -> Aca corregí un detalle, que es que en el tutorial se muestra el valor de score sin modificaciones, y yo decidí restarle los 4 "puntos" que se setean del largo de la serpiente porque me pareció lo mas lógico arrancar el puntaje desde cero.

--- script.js
+++ script.js
@@ -27,6 +27,29 @@ let boardSquares;
 let emptySquares;
 let moveInterval;
 
+const drawSnake = () => {
+    snake.forEach(square => drawSquare(square, 'snake'));
+}
+
+const drawSquare = (square, type) => {
+    const [row, column] = square.split('');
+    boardSquares[row][column] = squareTypes[type];
+    const squareElement = document.getElementById(square);
+    squareElement.setAttribute('class', `${type}Square`);
+
+    if (type === 'empty') {
+        emptySquares.push(square);
+    } else {
+        if(emptySquares.indexOf(square) !== -1) {
+            emptySquares.splice(emptySquares.indexOf(square), 1);
+        }
+    } 
+}
+
+const updateScore = () => {
+    scoreBoard.innerHTML = `Score: ${score - 4}`;
+}
+
 createBoard = () => {
     boardSquares.forEach((row, rowIndex) => {
     row.forEach( (column, columnIndex) => {
@@ -44,8 +67,9 @@ const setGame = () => {
     snake = ['00', '01', '02', '03'];
     score = snake.length;
     direction = 'ArrowRight';
-    boardSquares = Array.from(Array(boardSize), () => new Array(boardSize).fill(squareTypes.empty));
-    console.log(boardSquares);
+    boardSquares = Array.from(
+        Array(boardSize),
+        () => new Array(boardSize).fill(squareTypes.empty));
     board.innerHTML = '';
     emptySquares = [];
     createBoard();
@@ -53,6 +77,10 @@ const setGame = () => {
 
 const startGame = () => {
     setGame();
+    gameOver.style.display = 'none';
+    startButton.disabled = true;
+    drawSnake();
+    updateScore();
 }
 
 startButton.addEventListener('click', startGame);
