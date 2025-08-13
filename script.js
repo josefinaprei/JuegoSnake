ad1bbd2aa9a2cbba0bec517032233d5ede4b7e45
Josefina Preiser <134456477+josefinaprei@users.noreply.github.com>
2025-08-13 14:25:27 -0300

    Tutorial finalizado. Hay errores que corregir (se traba el juego despues de que la serpiente come 7 puntos. Entre otros detalles a mejorar.

--- script.js
+++ script.js
@@ -46,8 +46,72 @@ const drawSquare = (square, type) => {
     } 
 }
 
+
+const moveSnake = () => {
+    const newSquare = String(
+        Number(snake[snake.length - 1]) + directions[direction])
+        .padStart(2, '0');
+        const [row, column] = newSquare.split('');
+    if (newSquare<0 ||
+        newSquare > boardSize * boardSize ||
+        (direction === 'ArrowRight' && column == 0) ||
+        (direction === 'ArrowLeft' && column == boardSize - 1) ||
+        boardSquares[row][column] === squareTypes.snake) {
+            gameOver();
+        } else {
+            snake.push(newSquare);
+            if (boardSquares[row][column] === squareTypes.food) {
+                addFood();
+            } else {
+                const emptySquare = snake.shift();
+                drawSquare(emptySquare, 'empty');
+            }
+            drawSnake();
+        }      
+}
+
+const gameOver = () => {
+    gameOverSign.style.display = 'block';
+    clearInterval(moveInterval);
+    startButton.disabled = false;
+}
+
+const addFood = () => {
+    score++;
+    updateScore();
+    createRandomFood();
+}
+
+const createRandomFood = () => {
+    if (emptySquares.length > 0) {
+        const randomEmptySquare = emptySquares[Math.floor(Math.random() * emptySquares.length)];
+        drawSquare(randomEmptySquare, 'food');    
+    }
+}
+
 const updateScore = () => {
     scoreBoard.innerHTML = `Score: ${score - 4}`;
+}
+
+const setDirection = newDirection => {
+    direction = newDirection;
+}
+
+const directionEvent = key => {
+    switch(key.code) {
+        case 'ArrowUp':
+            direction != 'ArrowDown' && setDirection(key.code);
+        break;
+        case 'ArrowDown':
+            direction != 'ArrowUp' && setDirection(key.code);
+        break;
+        case 'ArrowLeft':
+            direction != 'ArrowRight' && setDirection(key.code);
+        break;
+        case 'ArrowRight':
+            direction != 'ArrowLeft' && setDirection(key.code);
+        break;
+    }
 }
 
 createBoard = () => {
@@ -77,10 +141,13 @@ const setGame = () => {
 
 const startGame = () => {
     setGame();
-    gameOver.style.display = 'none';
+    gameOverSign.style.display = 'none';
     startButton.disabled = true;
     drawSnake();
     updateScore();
+    createRandomFood();
+    document.addEventListener('keydown', directionEvent);
+    moveInterval = setInterval( () => moveSnake(), gameSpeed);
 }
 
 startButton.addEventListener('click', startGame);
